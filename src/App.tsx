b52c471de9f876d139c38531ab3b4dae4c7752a9
Chandanpreet Kaur <worldchampion202428@gmail.com>
2025-04-13 11:03:38 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import { CartProvider } from './context/CartContext';
+
+function App() {
+  return (
+    <Router>
+      <CartProvider>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+          <Toaster position="bottom-center" />
+        </div>
+      </CartProvider>
+    </Router>
+  );
+}
+
+export default App;
